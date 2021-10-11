14e4d709a21c88c04addae86214b9b23d2e48f52
Ayush783 <ayushc45xxx@gmail.com>
2021-10-11 22:33:29 +0530

    added bahi khata file removed web and test files

---
+++ lib/bahi_khata.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BahiKhata extends StatelessWidget {
+  const BahiKhata({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
