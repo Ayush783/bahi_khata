f53914aeba36d787f7613bcf3085e6226bbccf35
Ayush783 <ayushc45xxx@gmail.com>
2021-10-18 20:10:31 +0530

    added constant textstyles

---
+++ lib/constants/textstyles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kTextstyle1 = TextStyle(
+  fontSize: 24,
+  fontFamily: 'proza',
+  color: Colors.white,
+);
+
+const kTextstyle2 = TextStyle(
+  fontSize: 18,
+  fontFamily: 'ptsans',
+  color: Colors.white,
+);
