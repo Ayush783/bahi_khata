e44386ceb563d235377bc52ca77dab17e99aff19
Ayush783 <ayushc45xxx@gmail.com>
2021-10-18 20:05:47 +0530

    set ui mode and orientation in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'bahi_khata.dart';
 
 void main() {
-  runApp(BahiKhata());
+  WidgetsFlutterBinding.ensureInitialized();
+  //setting status bar and navigation bar colors
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Color(0xff252525),
+      systemNavigationBarColor: Color(0xfff0f0f0),
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
+  //only portrait mode
+  SystemChrome?.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(BahiKhata()));
 }
